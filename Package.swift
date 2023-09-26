e0b6a70277e079f36b583d004d3e4c2a8b442947
Franklin Byaruhanga <byaruhaf@protonmail.com>
2023-09-27 02:35:48 +0300

    improved name of package

--- Package.swift
+++ Package.swift
@@ -3,19 +3,8 @@
 
 import PackageDescription
 
-//let package = Package(
-//    name: "dropbox-sign-swift",
-//    targets: [
-//        // Targets are the basic building blocks of a package, defining a module or a test suite.
-//        // Targets can depend on other targets in this package and products from dependencies.
-//        .executableTarget(
-//            name: "dropbox-sign-swift"),
-//    ]
-//)
-
-
 let package = Package(
-    name: "dropbox-sign-swift",
+    name: "HelloSignClient",
     platforms: [
         .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
     ],
@@ -26,7 +15,7 @@ let package = Package(
     ],
     targets: [
         .executableTarget(
-            name: "dropbox-sign-swift",
+            name: "HelloSignClient",
             dependencies: [
                 .product(
                     name: "OpenAPIRuntime",
